3d755252b3c5495d208956e4a0271a1cf0e6bc41
Mageswaran4363 <mageswaran4363@gmail.com>
2023-08-24 17:27:18 +0530

    entire code in css and html

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 function myFunction() {
   alert("Button clicked!");
 }
+document.addEventListener("DOMContentLoaded", function() {
+  // Add click event listeners to the buttons
+  const shotButton = document.getElementById("shot-button");
+  if (shotButton) {
+    shotButton.addEventListener("click", function() {
+      alert("Shot button clicked!");
+    });
+  }
+});
